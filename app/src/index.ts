fc4b5649dbcf52ae83448cd450958ece510ea9ef
Joshua Young <djry1999@gmail.com>
2021-10-30 14:11:56 +1000

    Implement routing controllers. Set up DB.

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,15 +1,49 @@
 import * as express from 'express';
-import { router as indexRouter } from './routes/index';
+import * as mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { useExpressServer } from 'routing-controllers';
+import 'reflect-metadata';
 
+/**
+ * Class for application entry-point
+ */
 class App {
   public express = express.application;
   private port = 3000;
 
   constructor() {
     this.express = express();
-    this.express.use('/', indexRouter);
+    this.configureApp();
+    this.startDBServer();
+    // Register created express server in routing-controllers
+    useExpressServer(this.express, {
+      cors: true,
+      controllers: [`${__dirname}/controllers/*.ts`],
+    });
     this.express.listen(this.port);
   }
+
+  /**
+   * Method to configure express app
+   */
+  private configureApp(): void {
+    this.express.use(express.json());
+    this.express.use(express.urlencoded({ extended: true }));
+  }
+
+  /**
+   * Method to start MongoDB in-memory instance
+   */
+  private async startDBServer(): Promise<void> {
+    const mongoServer = await MongoMemoryServer.create();
+    const uri = await mongoServer.getUri();
+    mongoose.connect(uri);
+    // Close instance on SIGTERM
+    process.on('SIGTERM', () => {
+      mongoose.connection.close(false);
+    });
+  }
 }
+
 
 export default new App().express;
